6afafa8e3b2a631b7dd1b19039d3eb65f5e128b4
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2025-08-19 20:19:33 +0530

    feat: initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="bg-blue-500 text-white p-4 m-4">
+    <h1 class="text-4xl font-bold">This should be blue with white text</h1>
+    <p class="text-red-500 bg-yellow-200 p-2 mt-2">This should be red text on yellow background</p>
+  </div>
 </template>
-
 <style scoped></style>
